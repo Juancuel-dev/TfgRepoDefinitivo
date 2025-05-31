29fc0487278a700f246167d1bfaf510e3b7bdc29
jcuellog01 <jcuellog01@educarex.es>
2025-05-31 21:08:02 +0200

    frontend deploy test

---
+++ frontend/server.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() async {
+  final handler = createStaticHandler('build/web', defaultDocument: 'index.html');
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await io.serve(handler, '0.0.0.0', port);
+  print('✅ Servidor iniciado en http://localhost:${server.port}');
+}
