602d29996563fb4623dadd26f5e54ec8bfda3eb0
juan cuello gutierrez <jcuellog01@educarex.es>
2025-06-03 09:01:28 +0200

    fix puerto frontend despliegue

--- frontend/server.dart
+++ frontend/server.dart
@@ -4,7 +4,7 @@ import 'package:shelf_static/shelf_static.dart';
 
 void main() async {
   final handler = createStaticHandler('build/web', defaultDocument: 'index.html');
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '56000');
   final server = await io.serve(handler, '0.0.0.0', port);
   print('✅ Servidor iniciado en http://localhost:${server.port}');
 }
